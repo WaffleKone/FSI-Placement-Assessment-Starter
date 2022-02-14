7d0d2b1ea414a4b0900744b853ad60b04a80d1e6
Waffle <88282807+WaffleKone@users.noreply.github.com>
2022-02-14 18:24:50 -0500

    Added functionality to buttons to update variables in the js file

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,66 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Jaxon Johnson"
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-// selecting the element with an id of credit
+// Declaring button variables
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
+const gbPlusBtn = document.querySelector('#add-gb')
+const gbMinusBtn = document.querySelector('#minus-gb')
+
+const ccPlusBtn = document.querySelector('#add-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
+
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    if (gb >= 0){
+        gb += 1
+        console.log(gb)
+    }
 })
+
+gbMinusBtn.addEventListener('click', function(){
+    if (gb > 0){
+        gb -= 1
+        console.log(gb)
+    }
+})
+
+ccPlusBtn.addEventListener('click', function() {
+    if (cc >= 0){
+        cc += 1
+        console.log(cc)
+    }
+})
+
+ccMinusBtn.addEventListener('click', function(){
+    if (cc > 0){
+        cc -= 1
+        console.log(cc)
+    }
+})
+
+sugarPlusBtn.addEventListener('click', function() {
+    if (sugar >= 0){
+        sugar += 1
+        console.log(sugar)
+    }
+})
+
+sugarMinusBtn.addEventListener('click', function(){
+    if (sugar > 0){
+        sugar -= 1
+        console.log(sugar)
+    }
+})
+
 
 // TODO: Hook up event listeners for the rest of the buttons
