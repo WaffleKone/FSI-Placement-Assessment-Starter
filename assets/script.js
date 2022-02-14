e3e5f649740432f67ef6b4c473f816a3b8a8aede
Waffle <88282807+WaffleKone@users.noreply.github.com>
2022-02-14 18:37:57 -0500

    Made buttons appropriately update tables on the screen

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,16 @@ let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-// Declaring button variables
+// Declaring variables to ref elements in the order summary table
+const gbQty = document.querySelector('#qty-gb')
+const ccQty = document.querySelector('#qty-cc')
+const sugarQty = document.querySelector('#qty-sugar')
+const totalQty = document.querySelector('#qty-total')
+
+// Declaring variable that will be used to update the footer credit section
 const credit = document.querySelector('#credit')
 
+// Declaring button variables
 const gbPlusBtn = document.querySelector('#add-gb')
 const gbMinusBtn = document.querySelector('#minus-gb')
 
@@ -18,47 +25,52 @@ const sugarPlusBtn = document.querySelector('#add-sugar')
 const sugarMinusBtn = document.querySelector('#minus-sugar')
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
-
-// Event listener for clicks on the "+" button for Gingerbread cookies
+// Listeners for all of the gb, cc, and sugar + and - buttons
 gbPlusBtn.addEventListener('click', function() {
     if (gb >= 0){
         gb += 1
-        console.log(gb)
+        gbQty.textContent = (gb)
+        totalQty.textContent = (gb+cc+sugar)
     }
 })
 
 gbMinusBtn.addEventListener('click', function(){
     if (gb > 0){
         gb -= 1
-        console.log(gb)
+        gbQty.textContent = (gb)
+        totalQty.textContent = (gb+cc+sugar)
     }
 })
 
 ccPlusBtn.addEventListener('click', function() {
     if (cc >= 0){
         cc += 1
-        console.log(cc)
+        ccQty.textContent = (cc)
+        totalQty.textContent = (gb+cc+sugar)
     }
 })
 
 ccMinusBtn.addEventListener('click', function(){
     if (cc > 0){
         cc -= 1
-        console.log(cc)
+        ccQty.textContent = (cc)
+        totalQty.textContent = (gb+cc+sugar)
     }
 })
 
 sugarPlusBtn.addEventListener('click', function() {
     if (sugar >= 0){
         sugar += 1
-        console.log(sugar)
+        sugarQty.textContent = (sugar)
+        totalQty.textContent = (gb+cc+sugar)
     }
 })
 
 sugarMinusBtn.addEventListener('click', function(){
     if (sugar > 0){
         sugar -= 1
-        console.log(sugar)
+        sugarQty.textContent = (sugar)
+        totalQty.textContent = (gb+cc+sugar)
     }
 })
 
